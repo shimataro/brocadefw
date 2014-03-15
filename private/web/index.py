a3b07b1f283fd1faa75207b99fa2d14976d8abc2
shimataro <shimataro@zelkova.cc>
2014-03-16 07:42:37 +0900

    A: usage of i18n module

--- private/web/index.py
+++ private/web/index.py
@@ -6,5 +6,13 @@ from private import handler
 class Handler(handler.MyBaseHandler):
 	def on_get(self):
 		""" リクエスト処理 """
+
+		# i18nモジュールの使用例
+		from brocade.i18n import I18n
+		i18n = I18n()
+		labels = i18n.labels()
+		print(i18n.message(labels.NAME))
+		print(i18n.message(labels.MYNAME, {"name": "shimataro"}))
+
 		template = self.create_template_html()
 		return template.render("index.html")
