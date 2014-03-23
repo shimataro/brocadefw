ffb226fbc69f05a26499b8ec582cc8c1e7125a54
shimataro <shimataro@zelkova.cc>
2014-03-23 12:13:32 +0900

    M: create method for i18n

--- private/web/index.py
+++ private/web/index.py
@@ -6,13 +6,17 @@ from private import handler
 class Handler(handler.MyBaseHandler):
 	def on_get(self):
 		""" リクエスト処理 """
-
-		# i18nモジュールの使用例
-		from brocade.i18n import I18n
-		i18n = I18n()
-		labels = i18n.labels()
-		print(i18n.message(labels.NAME))
-		print(i18n.message(labels.MYNAME, {"name": "shimataro"}))
+#		self.__test_i18n()
 
 		template = self.create_template_html()
 		return template.render("index.html")
+
+
+	def __test_i18n(self):
+		""" i18nモジュールの使用例 """
+		from brocade.i18n import I18n
+
+		i18n = I18n(accept_languages = self.parse_accept("Language"))
+		labels = i18n.labels()
+		print(i18n.message(labels.NAME))
+		print(i18n.message(labels.MYNAME, {"name": "shimataro"}))
