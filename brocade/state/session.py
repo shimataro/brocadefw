7aede2e41d95baebbb5d803edd0e531aa9d23fc5
shimataro <shimataro@zelkova.cc>
2014-07-27 12:17:55 +0900

    A: added session.DictSaver

--- brocade/state/session.py
+++ brocade/state/session.py
@@ -13,9 +13,7 @@ def generate_id():
 
 
 class Saver(object):
-	""" セッションセーバ
-	（保存先に応じて継承すること）
-	"""
+	""" セッションセーバ（保存先に応じて継承すること） """
 
 	def load(self, session_id):
 		""" セッションデータをロード
@@ -37,21 +35,25 @@ class Saver(object):
 
 
 
-class MemcachedSaver(Saver):
-	""" memcachedを使用したセッションセーバ
+class DictSaver(Saver):
+	""" 辞書を使用したセッションセーバ（メモリを圧迫した際の削除機構を持たないので、本番環境では使わないこと） """
 
-	@requires: https://pypi.python.org/pypi/python3-memcached/
-	@requires: https://pypi.python.org/pypi/python-memcached/
-	"""
-	def __init__(self, servers = ["127.0.0.1:11211"], prefix = "session:"):
-		import memcache
-		self.__memcached = memcache.Client(servers)
+	# スレッドセーフにするためのロック機構
+	from threading import Lock
+	__lock = Lock()
+
+	# キャッシュは全インスタンスで共有
+	__cache = {}
+
+	def __init__(self, prefix = "session:"):
 		self.__prefix = prefix
 	
 	
 	def load(self, session_id):
 		key = self.__prefix + session_id
-		data = self.__memcached.get(key)
+		with self.__lock:
+			data = self.__cache.get(key, None)
+
 		if data == None:
 			# データがなければ空の辞書を返す
 			data = {}
@@ -61,4 +63,33 @@ class MemcachedSaver(Saver):
 
 	def save(self, session_id, data, lifetime):
 		key = self.__prefix + session_id
-		self.__memcached.set(key, data, time = lifetime)
+		with self.__lock:
+			self.__cache[key] = data
+
+
+
+class MemcachedSaver(Saver):
+	""" memcachedを使用したセッションセーバ（本番環境で使うならこっち）
+
+	@requires: https://pypi.python.org/pypi/python3-memcached/
+	@requires: https://pypi.python.org/pypi/python-memcached/
+	"""
+	def __init__(self, servers = ["127.0.0.1:11211"], prefix = "session:"):
+		import memcache
+		self.__cache = memcache.Client(servers)
+		self.__prefix = prefix
+	
+	
+	def load(self, session_id):
+		key = self.__prefix + session_id
+		data = self.__cache.get(key)
+		if data == None:
+			# データがなければ空の辞書を返す
+			data = {}
+
+		return data
+
+
+	def save(self, session_id, data, lifetime):
+		key = self.__prefix + session_id
+		self.__cache.set(key, data, time = lifetime)
--- private/handler.py
+++ private/handler.py
@@ -3,4 +3,7 @@
 from brocade import wsgi
 
 class MyBaseHandler(wsgi.WSGI_Handler):
-	pass
+	def session_saver(self):
+		""" セッションセーバを取得（DictSaverはテスト用なので、本番環境ではMemcachedSaver等を使うこと） """
+		from brocade.state import session
+		return session.DictSaver()
