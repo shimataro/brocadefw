bf18704a8802b8c26c35054fc48cbcafdf998daa
shimataro <shimataro@zelkova.cc>
2014-08-13 22:42:21 +0900

    A: strutils.to_bytes(), strutils.hash128()

--- brocadefw/utilities/strutils.py
+++ brocadefw/utilities/strutils.py
@@ -19,6 +19,25 @@ def autodecode(string, encodings = ("utf-8", "shift_jis", "euc_jp", "iso2022_jp"
 	return string
 
 
+def to_bytes(string):
+	""" 文字列をバイト列に変換
+	Python2の文字列型/Unicode型、Python3の文字列型に対応
+	（Python2の文字列型はそのまま返し、それ以外はUTF-8にエンコードする）
+
+	@param string: 文字列
+	@return: バイト列
+	"""
+	if not hasattr(string, "decode"):
+		# decodeメソッドがなければPython3の文字列型なのでバイト文字列にエンコードして返す
+		return string.encode("utf-8")
+
+	if type(string) != str:
+		# str型でなければPython2の文字列型
+		return string.encode("utf-8")
+
+	return string
+
+
 def validate_email(email):
 	""" Eメールアドレスのフォーマットを検証
 
@@ -33,3 +52,18 @@ def validate_email(email):
 		return False
 
 	return True
+
+
+def hash128(data):
+	""" 衝突の確率が極めて低い128bitのハッシュ値を生成
+	（単なるMD5ハッシュではない）
+
+	@param data: データを生成する文字列
+	@return: ハッシュ値（バイト列）
+	"""
+	from hashlib import md5
+
+	# 8回繰り返した文字列のMD5ハッシュを計算
+	# （8回繰り返した文字列同士のMD5ハッシュ値が衝突する確率は極めて少ないため）
+	h = md5(to_bytes(data * 8))
+	return h.digest()
